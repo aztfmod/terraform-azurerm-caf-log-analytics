833bc06d7e75bcc831706a5b077cd44f7492af05
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-11 18:07:31 +0800

    Documentation update

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "id" {
+  description = "Output the object ID"
   value      = azurerm_log_analytics_workspace.log_analytics.id
 }
 
 output "name" {
+  description = "Output the object name"
   value      = azurerm_log_analytics_workspace.log_analytics.name
 }
 
 output "object" {
+  description = "Output the full object"
   value      = azurerm_log_analytics_workspace.log_analytics
 }
